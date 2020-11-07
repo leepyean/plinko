e2f4b67f3f11ef70fc15ba9b462f3a273b74c7b8
leepyean <70707335+leepyean@users.noreply.github.com>
2020-11-07 20:15:53 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,26 @@
+class Particle {
+    constructor(x, y, r){
+        var options = {
+            restitution:0.2,
+            isStatic:false
+        }
+        this.r = r;
+        this.body = Bodies.circle(x,y,r,options);
+        stroke("black")
+        this.color = color(random(0,255), random(0,255), random(0,255))
+        World.add(world, this.body);
+        //this.color = color(random(0,255), random(0,255), random(0,255))
+    }
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+        fill(this.color)
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+      }
+}
